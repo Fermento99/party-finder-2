6d6cdf6e0d8acd5aaa2493437da1d70c0469e895
Paweł Kajanek <pawel.kajanek@stxnext.pl>
2025-05-28 15:11:44 +0200

    fix festival api

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,8 +4,13 @@ from .spotify_api.band import get_bands_details
 from .spotify_api.user import get_user_details
 
 class FestivalSerializer(serializers.ModelSerializer):
-    bands = serializers.SerializerMethodField()
     users = serializers.SerializerMethodField()
+    bands = serializers.SerializerMethodField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if type(self.context['view']).__name__ == 'FestivalListCreate' :
+            self.fields.pop('bands')
     
     class Meta:
         model = Festival
@@ -14,13 +19,28 @@ class FestivalSerializer(serializers.ModelSerializer):
     def get_bands(self, obj):
         access_token = self._context['request'].COOKIES['access_token']
         band_entries = BandEntry.objects.filter(festival_id=obj.id)
-        return get_bands_details([band_entry.band.spotify_id for band_entry in band_entries], access_token)
+        band_data = get_bands_details([band_entry.band.spotify_id for band_entry in band_entries], access_token)
+        for band in band_data:
+            band['votes'] = [VoteSerializer(vote).data for vote in Vote.objects.filter(band_id=band['id'])]
+        return band_data
     
     def get_users(self, obj):
-        access_token = self._context['request'].COOKIES['access_token']
         user_entries = UserEntry.objects.filter(festival_id=obj.id)
+        return [UserEntrySerializer(user_entry).data for user_entry in user_entries]
+    
+class UserEntrySerializer(serializers.ModelSerializer):
+    user_nickname = serializers.SerializerMethodField()
+    user_status_display = serializers.SerializerMethodField()
 
-        return [{ **get_user_details(user.user.spotify_id, access_token), 'user_status': user.get_user_status_display() } for user in user_entries]
+    class Meta:
+        model = UserEntry
+        fields = ['festival_id', 'user_id', 'user_nickname', 'user_status_display', 'user_status']
+
+    def get_user_nickname(self, obj):
+        return obj.user.nickname
+    
+    def get_user_status_display(self, obj):
+        return obj.get_user_status_display()
 
 
 class BandSerializer(serializers.ModelSerializer):
@@ -36,14 +56,21 @@ class BandSerializer(serializers.ModelSerializer):
         return get_bands_details([obj.spotify_id], access_token)[0]
     
     def get_votes(self, obj):
-        return [serialize_vote(vote) for vote in Vote.objects.filter(band_id=obj.spotify_id)]
+        return [VoteSerializer(vote).data for vote in Vote.objects.filter(band_id=obj.spotify_id)]
 
-def serialize_vote(vote):
-    return {
-        'user_id': vote.user.spotify_id,
-        'user_nickname': vote.user.nickname,
-        'vote': vote.get_vote_display(),
-    }
+class VoteSerializer(serializers.ModelSerializer):
+    user_nickname = serializers.SerializerMethodField()
+    vote_display = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Vote
+        fields = ['band_id', 'user_id','user_nickname', 'vote', 'vote_display']
+
+    def get_user_nickname(self, obj):
+        return obj.user.nickname
+    
+    def get_vote_display(self, obj):
+        return obj.get_vote_display()
 
 class UserSerializer(serializers.ModelSerializer):
     details = serializers.SerializerMethodField()
